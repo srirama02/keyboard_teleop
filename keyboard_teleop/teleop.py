5d5faa5d0090a62dac0c42f1b30d471b2ae390bc
Sriram Ashokkumar <ashokkumar2@wisc.edu>
2023-10-23 14:18:31 -0500

    cobra compatible

--- keyboard_teleop/teleop.py
+++ keyboard_teleop/teleop.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 from geometry_msgs.msg import Twist, TwistStamped
 from rclpy.node import Node
 from rclpy.qos import qos_profile_system_default
+from chrono_ros_interfaces.msg import CobraSpeedDriver
 
 
 class Teleop(Node, ABC):
@@ -23,16 +24,21 @@ class Teleop(Node, ABC):
 
         self._robot_base_frame = self.get_parameter("robot_base_frame").value
 
-        if self.get_parameter("twist_stamped_enabled").value:
-            self.publisher_ = self.create_publisher(
-                TwistStamped, "cmd_vel", qos_profile_system_default
+        # if self.get_parameter("twist_stamped_enabled").value:
+        #     self.publisher_ = self.create_publisher(
+        #         TwistStamped, "cmd_vel", qos_profile_system_default
+        #     )
+        #     self._make_twist = self._make_twist_stamped
+        # else:
+        #     self.publisher_ = self.create_publisher(
+        #         Twist, "cmd_vel", qos_profile_system_default
+        #     )
+        #     self._make_twist = self._make_twist_unstamped
+
+        self.publisher_ = self.create_publisher(
+                CobraSpeedDriver, "/chrono_ros_node/input/driver_inputs", qos_profile_system_default
             )
-            self._make_twist = self._make_twist_stamped
-        else:
-            self.publisher_ = self.create_publisher(
-                Twist, "cmd_vel", qos_profile_system_default
-            )
-            self._make_twist = self._make_twist_unstamped
+
         rate = 1 / self.get_parameter("publish_rate").value
         self.create_timer(rate, self._publish)
         self.linear = 0.0
@@ -72,12 +78,20 @@ class Teleop(Node, ABC):
         twist_stamped.twist = self._make_twist_unstamped(linear, angular)
         return twist_stamped
 
+    def _cobra_speed(self, linear, angular):
+        cobra_msg = CobraSpeedDriver()
+        cobra_msg.steering = angular
+        cobra_msg.speed = linear
+        return cobra_msg
+
     def _publish(self):
-        twist = self._make_twist(self.linear, self.angular)
-        self.publisher_.publish(twist)
+        # twist = self._make_twist(self.linear, self.angular)
+
+        cobra_msg = self._cobra_speed(self.linear, self.angular)
+        self.publisher_.publish(cobra_msg)
 
     def _update_screen(self):
         sys.stdout.write(f"Linear: {self.linear:.2f}, Angular: {self.angular:.2f}\r")
 
     def _emergency_stop(self):
-        self.publisher_.publish(self._make_twist(0.0, 0.0))
+        self.publisher_.publish(self._cobra_speed(0.0, 0.0))
